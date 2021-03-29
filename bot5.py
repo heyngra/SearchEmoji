bbc276dfea24d1bb75942d1e8bb966f12cd13623
heyngra <heyngra.wspolpraca@gmail.com>
2021-03-29 22:38:52 +0200

    optimize
    yes

--- bot5.py
+++ bot5.py
@@ -20,83 +20,73 @@ client = commandclient = commands.Bot(command_prefix = '-')
 if os.path.isfile("config.yml") == False:
     print("Creating config file in your file location!")
     cfg = open("config.yml", 'w')
-    cfg.write("#Welcome to the config! Please read every line about config lines, because editing something wrong can harm this program!\n\n\n#Put your discord token bot right here:\nTOKEN: \"\"\n\n#Please here put your channel list. Each channel you should put in layout\"- ID\" every each line.\nIDs:\n - FirstID\n\n\n#Please put here your emoji. Format: <:emojiname:emoji_id>\nemoji: \"<>\"\n\n\n###################\n#Credit by heyngra#\n###################")
+    cfg.write("#Welcome to the config! Please read every line about config lines, because editing something wrong can harm this program!\n\n\n#Put your discord token bot right here:\nTOKEN: \"\"\n\n#Please here put your channel list. Each channel you should put in layout\"- ID\" every each line.\nIDs:\n - FirstID\n\n\n#Please put here your emoji. Format: <:emoji:emoji_id>\nemoji: \"<>\"\n\nautorun: False #Provide True if you want to check run by auto. When False, most of settings above DON'T MATTER. You provide them using command.\noffline: False #Should bot be marked as Offline or Online (in status). Writing True will turn it to offline. Anything else will disable this.\n###################\n#Credit by heyngra#\n###################")
     cfg.close()
     print("Done!\nPlease, now fill the configuration file with things and start this program again!")
     time.sleep(3)
     exit()
 elif os.path.isfile("config.yml") == True:
-    global TOKEN
-    global ids
-    global emojiname
+    global TOKEN, ids, emoji, autorun, offline
     with open("config.yml", "r") as ymlfile:
         cfg = yaml.safe_load(ymlfile)
         TOKEN = cfg["TOKEN"]
-        ids = cfg["IDs"]
-        emojiname = cfg["emoji"]
-
-def checkmessage(id1, id2):
-    for emojis in range(len(id1.reactions)):
-        if emojiname == str(id1.reactions[emojis]):
-            messagesslownik.append(id2)
-        else:
-            pass
+        ids = cfg["ids"]
+        emoji = cfg["emoji"]
+        autorun = cfg["autorun"]
+        offline = cfg["offline"]
 def transform(message):
-    for emojis in range(len(message.reactions)):
-        if emojiname == str(message.reactions[emojis]):
+    for emojis in message.reactions:
+        if emoji == str(emojis):
             global messagesslownik
             messagesslownik.append(message.author.id)
-            #return str(message.reactions[emojis])
             return str(message.author.name)
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.offline)
+    if offline == True:
+        await client.change_presence(status=discord.Status.offline)
     print("bot on")
-    global ids
-    global atime
-    global atime2
-    global atimepass
-    atime = int(time.time())
-    for aaa in range(len(ids)):
-        channel = await client.fetch_channel(ids[aaa])
-        print(channel.name)
-        async for content in channel.history(limit=None).map(transform):
-            if content == None:
-                pass
-            else:
-                f = open("osoby.list", "a")
-                print(content)
-                f.write(content)
-                f.close()
-    atime2 = int(time.time())
-    atimepass = atime2 - atime
-    print("Done in %s!" % str(datetime.timedelta(seconds=atimepass)))
+    if autorun == True:
+        global atime, atime2, atimepass
+        atime = int(time.time())
+        for aaa in ids:
+            channel = await client.fetch_channel(aaa)
+            print(channel.name)
+            async for content in channel.history(limit=None).map(transform):
+                if content == None:
+                    pass
+                else:
+                    f = open("osoby.list", "a")
+                    #print(content)
+                    f.write(content)
+                    f.close()
+        atime2 = int(time.time())
+        atimepass = atime2 - atime
+        print("Done in %s!" % str(datetime.timedelta(seconds=atimepass)))
 @client.command()
 async def reactions(ctx):
     osoby = []
-    print("zaczynam obliczać")
-    for x in range(len(messagesslownik)):
+    for x in messagesslownik:
         try:
-            osoby.append(messagesslownik[x])
+            osoby.append(x)
             print(osoby)
             try:
-                globals()[messagesslownik[x]]
+                globals()[x]
             except:
-                globals()[messagesslownik[x]] = 0
-                globals()[messagesslownik[x]] += 1
+                globals()[x] = 0
+                globals()[x] += 1
             else:
-                globals()[messagesslownik[x]] += 1
+                globals()[x] += 1
         except:
             pass
     osoby = list(dict.fromkeys(osoby))
     osobysorted = {}
-    for a in range(len(osoby)):
-        osobysorted[osoby[a]] = globals()[osoby[a]]
+    for a in osoby:
+        osobysorted[a] = globals()[a]
     print(osobysorted)
     osoby = sorted(osobysorted)
     print(osoby)
-    for y in range(len(osoby)):
-        osoba = osoby[y]
+    for y in osoby:
+        osoba = y
         ilosc.append("<@%s>: %i" % (osoba, globals()[osoba]))
     ilosc2 = '\n'.join(ilosc)
     x1 = len(ilosc2)
